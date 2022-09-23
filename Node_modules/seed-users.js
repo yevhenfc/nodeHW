c0973905048d55c2b949b4f8557293f5d8048ce4
yevhenfc <yevhenfc@gmail.com>
2022-09-23 22:29:01 +0300

    task 2. add class method

--- Node_modules/seed-users.js
+++ Node_modules/seed-users.js
@@ -1,33 +1,33 @@
 class Users {
-    constructor(firstName, lastName, email, login) {
+    constructor(firstName, lastName, email, login, password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.login = login
-        this.passwordHash = '';
-        this.created = '';
-        this.updated = ''
+        this.login = login;
+        this.password = password
+    }    
+    
+    newUser (firstName = this.firstName, lastName = this.lastName, email = this.email, login = this.login, password = this.password) {
+        function curDate() {
+            let curDate = new Date();
+            return curDate.getDay() + "/" + curDate.getMonth() + "/" + curDate.getFullYear() + " " + curDate.getHours() + ":" + curDate.getMinutes() + ":" + curDate.getSeconds();
+        }
+        const saltRounds = 10;
+        return {
+            firstName: firstName, lastName: lastName, email: email, login: login, password: password, 
+            passwordHash: bcrypt.hashSync(password, saltRounds),
+            created: curDate(),
+            updated: curDate()
+        }    
     }
 };
 
 const bcrypt = require('bcrypt');
 const users = [];
 
-curDate = () => {
-    let curDate = new Date();
-    return curDate.getDay() + "/" + curDate.getMonth() + "/" + curDate.getFullYear() + " " + curDate.getHours() + ":" + curDate.getMinutes() + ":" + curDate.getSeconds();
-};
-
-passwordHash = (password) => {
-    const saltRounds = 10;
-    return bcrypt.hashSync(password, saltRounds)
-};  
-
 for (let i = 1; i < 11; i++) {
-    users[i-1] = new Users('firstName'+`${i}`, 'lastName'+`${i}`,  'email'+`${i}`+'@mail.com', 'login'+`${i}`);
-    users[i-1].created = curDate();
-    users[i-1].updated = curDate();
-    users[i-1].passwordHash = passwordHash('password'+`${i}`);
+    let user = new Users('firstName'+`${i}`, 'lastName'+`${i}`,  'email'+`${i}`+'@mail.com', 'login'+`${i}`, 'password'+`${i}`);
+    users[i-1] = user.newUser();
 }
 
 module.exports = users;
